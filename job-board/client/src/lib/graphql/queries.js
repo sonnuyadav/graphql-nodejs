91eea09c9354736989e632b643ca5e564a071507
Sonu Yadav <sonnu.yadav@gmail.com>
2023-11-14 00:34:04 +0530

    Completed CRUD opration

--- job-board/client/src/lib/graphql/queries.js
+++ job-board/client/src/lib/graphql/queries.js
@@ -21,6 +21,20 @@ export async function getCompany(id) {
   return company;
 }
 
+export async function createJob({ title, description }) {
+  const mutation = gql`
+    mutation CreateJob($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+      }
+    }
+  `;
+  const { job } = await client.request(mutation, {
+    input: { title, description },
+  });
+  return job;
+}
+
 export async function getJob(id) {
   const query = gql`
     query JobById($id: ID!) {
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,11 +1,38 @@
+import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
-import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
+import { createJob, updateJob, deleteJob, getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 
 export const resolvers = {
     Query: {
-       company: (_root, { id }) => getCompany(id), 
-       job: (_root, { id }) => getJob(id),
+      company: async (_root, { id }) => {
+        const company = await getCompany(id);
+        if (!company) {
+          throw notFoundError('No Company found with id ' + id);
+        }
+        return company;
+      },
+      
+      job: async (_root, { id }) => {
+        const job = await getJob(id);
+        if (!job) {
+          throw notFoundError('No Job found with id ' + id);
+        }
+        return job;
+      },
        jobs: () => getJobs(),
+  },
+
+  Mutation: {
+    createJob: (_root, { input: { title, description } }) => {
+      const companyId = 'FjcJCHJALA4i'; // TODO set based on user
+      return createJob({ companyId, title, description });
+    },
+    deleteJob: (_root, { id }) => deleteJob(id),
+
+    updateJob: (_root, { input: { id, title, description } }) => {
+      return updateJob({ id, title, description });
+    },
+    
   },
 
   Company: {
@@ -18,6 +45,12 @@ export const resolvers = {
   },
   
 };
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  });
+}
 
   function toIsoDate(value) {
     return value.slice(0, 'yyyy-mm-dd'.length);
