3dc2ab9f056fa769a3b7c0dabe9ce057863ffde7
Sonu Yadav <sonnu.yadav@gmail.com>
2023-11-17 00:23:29 +0530

    added pagination using graphql

--- job-board/client/src/lib/graphql/hooks.js
+++ job-board/client/src/lib/graphql/hooks.js
@@ -16,9 +16,10 @@ export function useJob(id) {
     return { job: data?.job, loading, error: Boolean(error) };
   }
   
-  export function useJobs() {
+  export function useJobs(limit, offset) {
     const { data, loading, error } = useQuery(jobsQuery, {
-      fetchPolicy: 'network-only',
+        variables: { limit, offset },
+        fetchPolicy: 'network-only',
     });
     return { jobs: data?.jobs, loading, error: Boolean(error) };
   }
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,6 +1,6 @@
 import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
-import { createJob, updateJob, deleteJob, getJob, getJobs, getJobsByCompany } from './db/jobs.js';
+import { countJobs, createJob, updateJob, deleteJob, getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 
 export const resolvers = {
     Query: {
@@ -19,7 +19,11 @@ export const resolvers = {
         }
         return job;
       },
-       jobs: () => getJobs(),
+      jobs: async (_root, { limit, offset }) => {
+        const items = await getJobs(limit, offset);
+        const totalCount = await countJobs();
+        return { items, totalCount };
+      },
   },
 
   Mutation: {
@@ -59,7 +63,9 @@ export const resolvers = {
   },
   
   Job: {
-    company: (job) => getCompany(job.companyId),
+    company: (job, _args, { companyLoader }) => {
+      return companyLoader.load(job.companyId);
+    },
     date: (job) => toIsoDate(job.createdAt),
   },
   
